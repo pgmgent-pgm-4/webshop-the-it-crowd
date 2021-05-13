612f648b8ab686183e4e2820febf305fd9b5f660
pgm-cagravci <cagravci@student.arteveldehs.be>
2021-05-13 15:17:29 +0200

    endpoints db

--- src/ormConfig.js
+++ src/ormConfig.js
@@ -1,6 +1,12 @@
 import typeorm from 'typeorm';
-import PostSchema from './server/api/model/Post/PostSchema.js';
+import UserSchema from './server/api/model/User/UserSchema.js';
+import ProfileSchema from './server/api/model/Profile/ProfileSchema.js';
 import CategorySchema from './server/api/model/Category/CategorySchema.js';
+import ProductSchema from './server/api/model/Product/ProductSchema.js';
+import ReviewSchema from './server/api/model/Review/ReviewSchema.js';
+import OrderSchema from './server/api/model/Order/OrderSchema.js';
+import PaymentSchema from './server/api/model/Payment/PaymentSchema.js';
+import PromotionSchema from './server/api/model/Promotion/PromotionSchema.js';
 
 export default typeorm.createConnection({
     type: "better-sqlite3",
@@ -12,7 +18,13 @@ export default typeorm.createConnection({
     synchronize: true,
     logging: true,
     entities: [
-        PostSchema,
-        CategorySchema
+        UserSchema,
+        ProfileSchema,
+        CategorySchema,
+        ProductSchema,
+        ReviewSchema,
+        OrderSchema,
+        PaymentSchema,
+        PromotionSchema
     ]
 })
