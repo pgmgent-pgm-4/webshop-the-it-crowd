12c066436ae6909c31d9b724a38b0af341b41e53
pgm-bramcriel <bramcrie@student.arteveldehs.be>
2021-06-07 17:46:08 +0200

    hamburger functionality

--- docs/assets/js/index.js
+++ docs/assets/js/index.js
@@ -1,21 +1,31 @@
 import test from './helpers.js';
 import constants from './constants.js';
-import subfunctions from './subfunctions.js';
 
 (() => {
     const app = {
         init() {
             this.eventListeners();
             this.cacheElements();
+            this.hamburgerMenu();
             console.log('ik werk');
             console.log('test:::', test)
             console.log('test:::', constants)
         },
         eventListeners() {
-
+            
         },
         cacheElements() {
-
+            this.hamburger = document.querySelector('.nav__hamburger');
+            this.navList = document.querySelector('.nav__list');
+        },
+        hamburgerMenu() {
+            this.hamburger.addEventListener('click', (e) => {
+                if(this.navList.classList.contains('show-items')) {
+                    this.navList.classList.remove('show-items');
+                } else {
+                    this.navList.classList.add('show-items');
+                }
+            })
         }
     }
     app.init();
